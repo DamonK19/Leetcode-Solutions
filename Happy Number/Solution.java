8996a4e430464b3c549ab0ac40faf5821adb6321
DamonK19 <37820586+DamonK19@users.noreply.github.com>
2020-04-02 22:19:37 -0700

    Update Solution.java

--- Happy Number/Solution.java
+++ Happy Number/Solution.java
@@ -4,8 +4,6 @@ class Solution {
         Solution s = new Solution();
         int val = 0;
         
-        //set.add(n);
-        
         while(!set.contains(n)) {
             set.add(n);
             val = 0;
